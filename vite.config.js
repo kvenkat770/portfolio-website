6de1b8f075d9e0a90c902f679076d8100adb9b6c
kvenkat770 <145830304+kvenkat770@users.noreply.github.com>
2025-08-17 11:55:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change 'username' and 'repo-name' to your GitHub repo details
+// Replace "portfolio-website" with your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-website/'
+  base: '/portfolio-website/',  
 })
